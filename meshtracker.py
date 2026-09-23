22f3e60187fd782d3389387be87e0ee391faebd8
Twostap <tyler.ostapyk@umanitoba.ca>
2026-09-22 20:05:45 -0500

    Update meshtracker.py

--- meshtracker.py
+++ meshtracker.py
@@ -129,7 +129,10 @@ def indexingchanges():
                 unchangedtotal.append(unchanged)
                 htmldiff = "<div style='display:inline' id='MESHTable'><h3><a href='" + PMURI + "'>PMID " + PMIDrow + "</a></h3><div style='display:inline-flex'><div style='display:inline'>" + htmldiff + "</div>" + "<div style='display:inline; font-family:courier; margin-left:15px;'><table id='diffcalculations'><tr><td id='addedlabel'>Added:</td><td id='addedvalue'>" + added + "</td><tr><td id='removedlabel'>Removed:</td><td id='removedvalue'>" + removed + "</td></tr><tr><td id='unchangedlabel'>Unchanged:</td><td id='unchangedvalue'>" + unchanged + "</td></tr></table></div></div></div>"
                 HTMLTables.append(htmldiff)
-                meshunchanged = meshunchanged
+                if MESHFilter is None or MESHFilter == "":
+                    meshunchanged = "N/A"
+                    meshadded = "N/A"
+                    meshremoved = "N/A"
 
         DiffHTML = "".join(HTMLTables) 
         addedtotal = list(map(int, addedtotal))
