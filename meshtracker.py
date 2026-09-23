d231ea0066555425995be08262d9753097aa7556
Twostap <tyler.ostapyk@umanitoba.ca>
2026-09-22 19:00:11 -0500

    Update meshtracker.py

--- meshtracker.py
+++ meshtracker.py
@@ -80,10 +80,11 @@ def indexingchanges():
                 OGDesc = "Original (" + OGIndexing + ") " + FirstDateRevised + ""
                 NewDesc = "Revised (" + NewIndexing + ") " + SecondDateRevised + ""
                 htmldiff = difflib.HtmlDiff().make_table(OGMESH, NewMESH, fromdesc=OGDesc, todesc=NewDesc)
-                meshremovedcheck = '"diff_sub">' + MESHFilter + "</span>"
-                meshaddedcheck = '"diff_add">' + MESHFilter + "</span>"
+                meshremovedcheck = '"diff_sub">' + MESHFilter + '</span>'
+                meshaddedcheck = '"diff_add">' + MESHFilter + '</span>'
                 print(meshaddedcheck)
-                meshunchangedcheck = '"nowrap">' + MESHFilter + "</td>"
+                print(htmldiff)
+                meshunchangedcheck = '"nowrap">' + MESHFilter + '</td>'
                 if meshremovedcheck in htmldiff:
                     meshremoved +=1
                 if meshaddedcheck in htmldiff:
